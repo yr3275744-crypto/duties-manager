40b985e1b8aefa82e342260642fd938c97f90bb0
Ytshak_Raiyner <yr3275744@gmail.com>
2026-05-20 11:42:53 +0300

    add functions signatures o functions_signatures.py

---
+++ functions_signatures.py
@@ -0,0 +1,77 @@
+def create_soldier() -> dict:
+    pass
+
+
+def add_soldier(soldiers:list, soldier:dict) -> list:
+    pass
+
+
+def remove_soldier(soldiers:list, soldier_name:str) -> list:
+    pass
+
+
+def print_soldiers_list(soldiers:list) -> None:
+    pass
+
+
+def creat_duty() -> dict:
+    pass
+
+
+def add_duty_to_soldier(soldier:dict, duty:dict) -> dict:
+    pass
+
+
+def update_duty_ststus(soldier:dict, duty_name:str, status_name:str) -> str:
+    pass
+
+
+def get_status_name(statuses:list) -> str:
+    pass
+
+
+def if_status_name_is_uniqe(soldiers:list, status_name:str) -> None:
+    """raise Valuerror if the status name is already existes."""
+
+def print_soldier_duties(soldier:dict) -> None:
+    pass
+
+
+def get_soldier_name() -> str:
+    pass
+
+
+def check_validate_of_str(a_string:str) -> None:
+    """raise Valuerror if a_value is not str or not full."""
+
+
+def check_if_id_is_uniqe(soldiers:list, id:int) -> bool:
+    """raise Valuerror if the id is already existes."""
+
+
+def get_day_name(day_names:list) -> str:
+    pass
+
+
+def get_id(soldiers:list) -> int:
+    pass
+
+
+def print_menu() -> None:
+    pass
+
+
+def get_menu_choice() -> int:
+    pass
+
+
+def if_selection_is_within_range(value:int, selection_range:int) -> None:
+    """raise Valuerror if the value is outside the desired range."""
+
+
+def mange_duty_and_soldiers(statuses:list, days:list) -> None:
+    pass
+
+
+def main(statuses:list, days:list) -> None:
+    pass
